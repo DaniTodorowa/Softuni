f009c18fdee09b1641216002a053fdea9e57ebf2
DaniTodorowa <danitodorova2106@gmail.com>
2020-07-12 01:56:10 +0300

    ...

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
-
 cd  C:\Users\Dani Todorova\PycharmProjects\Exercises
 git config --global user.email "danitodorova2106@gmail.com"
 git add .
 read msg
 git commit -m $msg
 git push -u origin master
+cd
